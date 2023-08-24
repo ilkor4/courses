e90ec571898f2f466bab4ad58602bd0afc3de8cc
ilkor4 <kornelyuck.ilya@yandex.ru>
2023-08-24 19:46:03 +0300

    feat: Окончательный вариант меню

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -20,51 +20,62 @@ export default function Burger(props) {
           <div className='burger__close-button'
             onClick={ () => props.onClose() }>
           </div>
-          <nav className='burger__menu'>
-            <ul className='burger__links'>
-              <li>
-                <Link
+          <div className='burger__content'>
+            <nav className='burger__menu'>
+              <Link
                 onClick={ () => props.onClose() }
-                  to='#carousel'
-                  className='burger__link'>Тех-карты
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={ () => props.onClose() }
-                  to='#about'
-                  className='burger__link'>Описание
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={ () => props.onClose() }
-                  to='#reviews'
-                  className='burger__link'>Отзывы
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={ () => props.onClose() }
-                  to='#questions'
-                  className='burger__link'>Частые вопросы
+                to='#main'
+                className='burger__link'>
+                  <img className="header__logo" src={headerLogo} alt="Логотип кондитерского бренда" />
+              </Link>
+              <ul className='burger__links'>
+                <li>
+                  <Link
+                    onClick={ () => props.onClose() }
+                    to='#carousel'
+                    className='burger__link'>Тех-карты
                   </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={ () => props.onClose() }
-                  to='#contacts'
-                  className='burger__link'>Контакты
-                </Link>
-              </li>
+                </li>
+                <li>
+                  <Link
+                    onClick={ () => props.onClose() }
+                    to='#about'
+                    className='burger__link'>Описание
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={ () => props.onClose() }
+                    to='#reviews'
+                    className='burger__link'>Отзывы
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={ () => props.onClose() }
+                    to='#questions'
+                    className='burger__link'>Частые вопросы
+                    </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={ () => props.onClose() }
+                    to='#contacts'
+                    className='burger__link'>Контакты
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <ul className='burger__caption'>
+              <li className='burger__text'>Кондитерская мастерская "Cakes by Julia"</li>
+              <li className='burger__text'>г. Волгоград</li>
             </ul>
-          </nav>
-          <Link
-            onClick={ () => props.onClose() }
-            to='#main'
-            className='burger__link'>
-              <img className="header__logo" src={headerLogo} alt="Логотип кондитерского бренда" />
-          </Link>
+            <div className='burger__social-icons'>
+              <Link  to="https://wa.me/" target='_blank' rel='noreferrer'><div className='burger__social-icon burger__social-icon_whatsApp'></div></Link>
+              <Link  to="https://www.instagram.com/" target='_blank' rel='noreferrer'><div className='burger__social-icon burger__social-icon_inst'></div></Link>
+              <Link  to="https://t.me/ " target='_blank' rel='noreferrer'><div className='burger__social-icon burger__social-icon_telegram'></div></Link>
+            </div>
+          </div>
         </div>
     </div>
   )
